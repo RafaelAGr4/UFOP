01c10c3d8ab39e6cdce380b98564d101ec8a2bcf
unknown <raagranato@gmail.com>
2026-05-05 20:08:44 -0300

    feat: estrutura inicial do projeto

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "bib.hpp"
+
+int main() {
+    std::cout << "=== cpp-estudos-ufop ===" << std::endl;
+
+    std::cout << "Fatorial de 5: " << calcularFatorial(5) << std::endl;
+    std::cout << "Fibonacci de 7: " << calcularFibonacci(7) << std::endl;
+
+    return 0;
+}
