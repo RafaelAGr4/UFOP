efd2595a630881a7ea7bdd44c27dba5ca8d67eec
RafaelAGr4 <rafael.granato@aluno.ufop.edu.br>
2026-05-05 22:37:20 -0300

    feat: implementa calcularFatorial com testes aprovados

--- src/bib.cpp
+++ src/bib.cpp
@@ -14,16 +14,6 @@ long long calcularFatorial(int n) {
 }
 
 long long calcularFibonacci(int n) {
-    if (n < 0) {
-        throw std::invalid_argument("Fibonacci nao definido para negativos.");
-    }
-    if (n == 0) return 0;
-    if (n == 1) return 1;
-    long long a = 0, b = 1;
-    for (int i = 2; i <= n; i++) {
-        long long temp = a + b;
-        a = b;
-        b = temp;
-    }
-    return b;
+    // TODO: implementar
+    return -1;
 }
